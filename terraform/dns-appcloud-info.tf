4f812bc540a30b774bc8589431ab8eb45a5d1d6c
ww24 <ww24gm@gmail.com>
2016-12-09 17:22:55 +0900

    add CNAME record for test

--- terraform/dns-appcloud-info.tf
+++ terraform/dns-appcloud-info.tf
@@ -14,6 +14,16 @@ resource "google_dns_record_set" "cname_jk_appcloud_info" {
   ]
 }
 
+resource "google_dns_record_set" "cname_wp_appcloud_info" {
+  managed_zone = "${google_dns_managed_zone.appcloud_info.name}"
+  name         = "wp.${google_dns_managed_zone.appcloud_info.dns_name}"
+  type         = "CNAME"
+  ttl          = 300
+  rrdatas      = [
+    "wp-mizuno2-1123190130.ap-northeast-1.elb.amazonaws.com."
+  ]
+}
+
 resource "null_resource" "nameservers_appcloud_info" {
   triggers {
     name_servers = "${join("\n", google_dns_managed_zone.appcloud_info.name_servers)}"
