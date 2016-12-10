8011981cc94bdda246a3933e82ddc29b03a91263
Takenori Nakagawa <ww24gm@gmail.com>
2016-12-10 14:51:24 +0900

    enable gcs versioning

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,5 +1,15 @@
 resource "google_storage_bucket" "mocha_cloud_dns_tfstate" {
   name          = "${var.remote_state_bucket}"
-  location      = "asia-northeast1"
+  location      = "${var.region}"
   storage_class = "REGIONAL"
+
+  provisioner "local-exec" {
+    # Enable versioning (not supported by terraform)
+    # https://cloud.google.com/storage/docs/object-versioning
+    command = <<EOF
+gcloud auth activate-service-account --key-file account.json
+gsutil versioning set on "${var.remote_state_bucket}"
+gsutil versioning get "${var.remote_state_bucket}"
+EOF
+  }
 }
