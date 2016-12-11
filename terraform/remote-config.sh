15fd99373a48784c4000c055babe1a8121b715dc
Takenori Nakagawa <ww24gm@gmail.com>
2016-12-12 02:56:17 +0900

    use ww24/terraform step

--- terraform/remote-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-terraform remote config -backend=gcs \
-                        -backend-config="bucket=mocha-cloud-dns-tfstate" \
-                        -backend-config="path=terraform.tfstate" \
-                        -backend-config="project=mocha-cloud" \
-                        -backend-config="credentials=$(cat account.json)"
