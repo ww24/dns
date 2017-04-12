a7d4df00fbb399d36f4d99c110d7cf006dc698b5
ww24 <ww24gm@gmail.com>
2017-04-12 14:06:19 +0900

    fix

--- terraform/dns-appcloud-info.tf
+++ terraform/dns-appcloud-info.tf
@@ -14,13 +14,14 @@ resource "google_dns_record_set" "cname_jk_appcloud_info" {
   ]
 }
 
-resource "google_dns_record_set" "spf_auth_appcloud_info" {
+resource "google_dns_record_set" "txt_auth_appcloud_info" {
   managed_zone = "${google_dns_managed_zone.appcloud_info.name}"
   name         = "auth.${google_dns_managed_zone.appcloud_info.dns_name}"
-  type         = "SPF"
+  type         = "TXT"
   ttl          = 3600
   rrdatas      = [
-    "\"v=spf1\" \"mx\" \"include:_spf.firebasemail.com\" \"~all\""
+    "v=spf1 include:_spf.firebasemail.com ~all",
+    "firebase=coconut-ed350"
   ]
 }
 
@@ -30,17 +31,7 @@ resource "google_dns_record_set" "txt_smtpapi_domainkey_appcloud_info" {
   type         = "TXT"
   ttl          = 3600
   rrdatas      = [
-     "\"k=rsa;\" \"t=s;\" \"p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDPtW5iwpXVPiH5FzJ7Nrl8USzuY9zqqzjE0D1r04xDN6qwziDnmgcFNNfMewVKN2D1O+2J9N14hRprzByFwfQW76yojh54Xu3uSbQ3JP0A7k8o8GutRF8zbFUA8n0ZH2y0cIEjMliXY4W4LwPA7m4q0ObmvSjhd63O9d8z1XkUBwIDAQAB\""
-  ]
-}
-
-resource "google_dns_record_set" "txt_auth_appcloud_info" {
-  managed_zone = "${google_dns_managed_zone.appcloud_info.name}"
-  name         = "auth.${google_dns_managed_zone.appcloud_info.dns_name}"
-  type         = "TXT"
-  ttl          = 3600
-  rrdatas      = [
-     "firebase=coconut-ed350"
+     "k=rsa; t=s; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDPtW5iwpXVPiH5FzJ7Nrl8USzuY9zqqzjE0D1r04xDN6qwziDnmgcFNNfMewVKN2D1O+2J9N14hRprzByFwfQW76yojh54Xu3uSbQ3JP0A7k8o8GutRF8zbFUA8n0ZH2y0cIEjMliXY4W4LwPA7m4q0ObmvSjhd63O9d8z1XkUBwIDAQAB"
   ]
 }
 
