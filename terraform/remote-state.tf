8cd36f48ec2a38901a87da08fb48533e4e9f3a25
Takenori Nakagawa <ww24gm@gmail.com>
2018-03-08 02:52:04 +0900

    fix backend config

--- terraform/remote-state.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "tfstate" {
-    backend = "gcs"
-    config {
-        credentials = "${file("account.json")}"
-        bucket = "${var.remote_state_bucket}"
-        path = "${var.remote_state_path}"
-        project = "${var.project}"
-    }
-}
