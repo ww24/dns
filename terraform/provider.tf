28582439469680656f6e764a22e0d064c674476a
Takenori Nakagawa <ww24gm@gmail.com>
2018-03-09 00:48:34 +0900

    Fix backend settings

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,4 +2,9 @@ provider "google" {
   credentials = "${file("account.json")}"
   project     = "${var.project}"
   region      = "${var.region}"
+  version = "~> v1.6.0"
+}
+
+provider "null" {
+  version = "~> v1.0.0"
 }
